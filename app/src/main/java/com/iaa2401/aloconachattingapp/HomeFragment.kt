4c818799cdc47a9509a058f9f6e9ba3bbf940ba4
Shimon31 <shimonbhuiya02@gmail.com>
2024-10-09 17:08:21 +0600

    Get Data From Firebase

--- app/src/main/java/com/iaa2401/aloconachattingapp/HomeFragment.kt
+++ app/src/main/java/com/iaa2401/aloconachattingapp/HomeFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -15,33 +17,54 @@ import com.google.firebase.database.ValueEventListener
 import com.iaa2401.aloconachattingapp.databinding.FragmentHomeBinding
 
 
-class HomeFragment : Fragment(),UserAdapter.ItemClick {
+class HomeFragment : Fragment(), UserAdapter.ItemClick {
 
     lateinit var binding: FragmentHomeBinding
-    lateinit var userDB : DatabaseReference
+    lateinit var userDB: DatabaseReference
     lateinit var adapter: UserAdapter
 
 
-    var userList : MutableList<User> = mutableListOf()
+    private var currentUser: User? = null
+    private var auth = FirebaseAuth.getInstance()
+    lateinit var firebaseUser: FirebaseUser
+    private val bundle = Bundle()
+    var userList: MutableList<User> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-
         userDB = FirebaseDatabase.getInstance().reference
 
-        binding.logoutBtn.setOnClickListener {
 
-            val auth = FirebaseAuth.getInstance()
+
+
+
+        FirebaseAuth.getInstance().currentUser?.let {
+            firebaseUser = it
+        }
+
+        binding.profileBtn.setOnClickListener {
+
+            currentUser?.let {
+
+                bundle.putString("id",it.userId)
+                findNavController().navigate(R.id.action_homeFragment_to_profileFragment,bundle)
+
+            }
+
+
+        }
+
+
+        binding.logoutBtn.setOnClickListener {
 
             auth.signOut().apply {
 
                 findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
 
             }
-
 
 
         }
@@ -59,13 +82,22 @@ class HomeFragment : Fragment(),UserAdapter.ItemClick {
 
     private fun getAvailableUSer() {
 
-        userDB.child(DBNODES.USER).addValueEventListener( object : ValueEventListener{
+        userDB.child(DBNODES.USER).addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 userList.clear()
                 snapshot.children.forEach {
 
-                    val user : User = it.getValue(User::class.java)!!
-                    userList.add(user)
+                    val user: User = it.getValue(User::class.java)!!
+
+                    if (firebaseUser.uid != user.userId) {
+                        userList.add(user)
+                    } else {
+                        currentUser = user
+
+//                        setProfile()
+
+                    }
+
 
                 }
                 adapter.submitList(userList)
@@ -80,11 +112,18 @@ class HomeFragment : Fragment(),UserAdapter.ItemClick {
 
     }
 
+//    private fun setProfile() {
+//        currentUser?.let {
+//
+//            binding.profileBtn.load("https://www.google.com/imgres?q=sadek%20bhuiya%20shimon&imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D492760747028449&imgrefurl=https%3A%2F%2Fm.facebook.com%2F100088835840260&docid=59WDNV_ROTIDAM&tbnid=oIcxOx9tCECHkM&vet=12ahUKEwit-vOChIGJAxW38zgGHeLEAFgQM3oECEkQAA..i&w=1536&h=2048&hcb=2&itg=1&ved=2ahUKEwit-vOChIGJAxW38zgGHeLEAFgQM3oECEkQAA")
+//        }
+//    }
+
     override fun onItemClick(user: User) {
 
-        val bundle = Bundle()
-        bundle.putString("id",user.userId)
-        findNavController().navigate(R.id.action_homeFragment_to_profileFragment,bundle)
+
+        bundle.putString("id", user.userId)
+        findNavController().navigate(R.id.action_homeFragment_to_profileFragment, bundle)
 
     }
 }
